973274dec3e8008cc7d777d17028d47ec1b4ad5a
Chris Mckeown <chris.j.mckeown@hotmail.com>
2021-06-06 20:45:31 +1000

    redux thunk added

--- src/containers/app.container.js
+++ src/containers/app.container.js
@@ -6,40 +6,47 @@ import Scroll from '../components/scroll.component';
 import ErrorBoundary from '../components/error-boundary.component';
 import './app.container.css';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
+
 const mapStateToProps = (state) => {
   return {
-    searchField: state.searchField,
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
   };
 };
 const mapDispatchToProps = (dispatch) => {
   return {
     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots()),
   };
 };
 
 const App = (props) => {
-  const [robots, setRobots] = useState([]);
+  const { searchField, onSearchChange, robots, isPending, onRequestRobots } =
+    props;
+  // const [robots, setRobots] = useState([]);
   // const [searchField, setSearchField] = useState('');
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((responce) => responce.json())
-      .then((users) => setRobots(users));
+    onRequestRobots();
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then((responce) => responce.json())
+    //   .then((users) => setRobots(users));
     console.log(count);
-  }, [count]); // only run if count changes
+  }, [onRequestRobots, count]); // only run if count changes
 
   // const onSearchChange = (event) => {
   //   setSearchField(event.target.value);
   // };
 
-  const { searchField, onSearchChange } = props;
   const filteredRobots = robots.filter((robot) => {
     return robot.name.toLowerCase().includes(searchField.toLowerCase());
   });
 
-  return !robots.length ? (
+  return isPending ? (
     <h1>Loading...</h1>
   ) : (
     <div className='tc'>
